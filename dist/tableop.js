30c0ee8e8f1c6879ef005abddd38ae840ce2a70c
ykominami <ykominami@gmail.com>
2025-06-22 16:12:38 +0900

    fix

--- dist/tableop.js
+++ dist/tableop.js
@@ -1,10 +1,13 @@
 class Tableop {
-  static showRange(range){
+  static getRangeShape(range){
     const row = range.getRow()
     const col = range.getColumn()
     const height = range.getNumRows()
     const width = range.getNumColumns()
-    // Logger.log(`################# showRange row=${row} col=${col} height=${height} width=${width}`)
     return [row, col, height, width]
+  }
+  static showRangeShape(range){
+    const [row, col, height, width] = Tableop.getRangeShape(range)
+    Logger.log(`################# showRange row=${row} col=${col} height=${height} width=${width}`)
   }
 }
